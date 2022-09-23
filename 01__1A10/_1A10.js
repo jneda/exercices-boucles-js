807690fd01c13a4d01d5db1bf6796ac1f9ad6002
jdane <julien.dane@gmail.com>
2022-09-23 11:14:22 +0200

    Implémenté 2 premiers exercices

--- 01__1A10/_1A10.js
+++ 01__1A10/_1A10.js
@@ -1,3 +1,21 @@
 /* 1/ Ecrire une fonction printNumbers() pour afficher les nombres de 1 à 10
 La fonction utilise la boucle for.
 Résultat : 1 2 3 4 5 6 7 8 9 10 */
+
+function printNumbers() {
+  // on construit le HTML par concaténation
+  let html = '<ul class="list">';
+
+  // la boucle
+  for (let i = 1; i < 11; i++) {
+    html += `<li class="list">${i}</li>`;
+  }
+
+  html += "</ul>";
+  console.log(html);
+
+  // on écrit dans le document
+  document.write(html);
+}
+
+printNumbers();
--- 02_affiche2D/affiche2D.js
+++ 02_affiche2D/affiche2D.js
@@ -3,3 +3,30 @@ tableau à 2 dimensions en utilisant des boucles for imbriquées.
 Le tableau :
 Let myArray = [[1,2], [3,4],[5,6]] ;
 Résultat : 1 2 3 4 5 6 */
+
+function printArray(array) {
+  // construction du html par concaténation
+  let html = '<ul class="list style="border: 2px solid #fcf;">';
+
+  // boucle extérieure
+  for (const arrayInterieur of array) {
+    html += '<li class="list"><ul class="list"' +
+      'style="border: 2px solid #cfc;">';
+
+    // boucle intérieure
+    for (const elt of arrayInterieur) {
+      html += `<li class="list">${elt}</li>`;
+    }
+
+    html += "</ul>";
+  }
+
+  html += '</ul>';
+
+  // on n'oublie pas d'écrire dans le document
+  document.write(html);
+}
+
+// test
+
+printArray([[1, 2], [3, 4], [5, 6]]);
