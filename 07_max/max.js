4d59367f2f46edb4d040ddb0f5e97aa26ad159fc
jneda <julien.dane@gmail.com>
2022-09-23 15:03:50 +0200

    Implémenté ex. 7

--- 07_max/max.js
+++ 07_max/max.js
@@ -3,3 +3,39 @@ donné.
 Exemple :
 let myArray = [2, 45, 3, 67, 34, 567, 34, 345, 123] ;
 Résultat : 567 */
+
+function max(array) {
+  // préservons-nous de mauvaises surprises
+  array = array.map(elt => parseInt(elt));
+
+  let max = array[0];
+
+  for (let i = 1; i < array.length; i++) {
+    const actuel = array[i];
+    if (actuel > max) {
+      max = actuel;
+    }
+  }
+
+  return max;
+}
+
+// utilitaires de log
+
+const pStyle = (x) =>
+  `<p style="min-height: 1.2em; background-color: #444;` +
+  `padding: 0.2em 1em;">${x}</p>`;
+
+function log (x) {
+  const message = `[${x}] max : ${max(x)}`;
+  console.log(message);
+  document.write(pStyle(message));
+}
+
+// tests
+
+log([2, 45, 3, 67, 34, 567, 34, 345, 123]);
+log([]);
+log([-66, -42, 42, 66]);
+log([-66, -42, -42, -66]);
+log(["-66", "-42"]);
